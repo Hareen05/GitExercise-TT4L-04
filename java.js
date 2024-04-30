b3fd759b7e05a20a9df6a561cc8181de4827f71a
Hareen <1221108308@soffice.mmu.edu.my>
2024-04-30 14:50:06 +0800

    Added background image and logo

--- java.js
+++ java.js
@@ -3,3 +3,10 @@ let search = document.querySelector('.search-bar');
 document.querySelector('#search-icon').onclick = () =>{
   search.classList.toggle('active');
 }
+
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick = () =>{
+  user.classList.toggle('active');
+  search.classList.toggle('active');
+}
